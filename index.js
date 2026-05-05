6025d7575eac1714784d6d92d6f4ac1e1bf7db11
ilham115 <ilhammuhamad162@gmail.com>
2026-05-05 15:53:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('EduCare jalan');
+});
+
+app.listen(3000, () => {
+    console.log('Server jalan di port 3000');
+});
