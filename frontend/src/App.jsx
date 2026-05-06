71eaf0fa869769cecd54b22a78b6397da6467027
ilham115 <ilhammuhamad162@gmail.com>
2026-05-06 08:39:59 +0700

    add frontend react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Frontend Educare Running</h1>
+      <p>Frontend deployment berhasil menggunakan React + Vercel.</p>
+    </div>
+  )
+}
+
+export default App
